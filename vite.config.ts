cc82be86fa4cf7450153851244a769dc4e9e72bd
Gab2006 <tostigabriele6@gmail.com>
2026-08-06 23:30:24 +0200

    feat: implement PWA manifest and update application icons and metadata

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.png', 'icons/icon-192.png', 'icons/icon-512.png'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.png'],
       manifest: {
         name: 'OpenBible',
         short_name: 'OpenBible',
@@ -18,15 +18,26 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'icons/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
           },
           {
-            src: 'icons/icon-512.png',
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-          },
+            purpose: 'maskable'
+          }
         ],
       },
       workbox: {
